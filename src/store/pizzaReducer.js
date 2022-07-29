2c9e3852b605dda94db3d0601b3405540aa98c33
Edgar Klepek <edgarklepek@gmail.com>
2022-07-29 15:17:45 +0200

    Add redux and fetching of data

--- src/store/pizzaReducer.js
+++ src/store/pizzaReducer.js
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   name: "pizza",
-// };
-
-const initialState = {
-  name: "pizza",
-};
+const initialState = [
+  { name: "Pizza Margherita", price: 4 },
+  { name: "Pizza Salami", price: 5 },
+  { name: "Pizza Tonno", price: 6 },
+];
 
 export const pizzaSlice = createSlice({
   name: "pizza",
