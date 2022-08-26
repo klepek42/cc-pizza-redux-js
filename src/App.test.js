4cbf161a69ef4eb99874924579a1ced15f0a6a7b
Edgar Klepek <edgarklepek@gmail.com>
2022-08-26 10:28:10 +0200

    Add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "react-dom";
+import { screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import { renderWithRedux } from "./utils/testUtils";
+
+describe("App", () => {
+  it("should test", () => {
+    render(<App />);
+    expect(screen.getByText("Pizza")).toBeDefined();
+  });
+  // it("renders initial pizza data from database", () => {
+  //   renderWithRedux(<App />, {});
+  //   expect(screen.getByText("Margherita")).toBeDefined();
+  // });
 });
